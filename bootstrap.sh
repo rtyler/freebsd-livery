e092dc5eb5e794028b8c58da26f82db98952b01f
R. Tyler Croy <tyler@monkeypox.org>
2012-07-08 16:34:36 -0700

    Add a bootstrap.sh script to set up our FreeBSD instance with pkgng and get it ready

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -x
+
+which pkg
+
+if [ $? -ne 0 ]; then
+  echo "pkgng is not installed!"
+  pkg_add ./pkg.tbz
+
+  echo "PACKAGESITE : http://pkgbeta.freebsd.org/freebsd-9-amd64/latest/" > /usr/local/etc/pkg.conf
+
+  pkg update
+  # Update pkgng itself
+  pkg install -y pkg
+  # Install rsync(1) so we don't have to fall back to tar(1)+scp(1) ever again
+  pkg install -y rsync
+fi
+
+pkg help
